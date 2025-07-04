eeffd6e9bca666a25e227639d7f09c12c31a7971
96allan <66323368+96allan@users.noreply.github.com>
2025-07-04 09:06:43 +0300

    Update config.php

--- app/config.php
+++ app/config.php
@@ -1,9 +1,13 @@
 <?php
-$host = getenv('MYSQL_HOST');
-$user = getenv('MYSQL_USER');
-$pass = getenv('MYSQL_PASSWORD');
-$db   = getenv('MYSQL_DATABASE');
+$host = getenv('MYSQL_HOST') ?: 'localhost';
+$user = getenv('MYSQL_USER') ?: 'root';
+$pass = getenv('MYSQL_PASSWORD') ?: 'root';
+$db   = getenv('MYSQL_DATABASE') ?: 'ecodrive';
+$port = getenv('MYSQL_PORT')  ?: 3306;
 
+$conn = new mysqli($host, $user, $pass, $db, $port);
+
+//check connection
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
